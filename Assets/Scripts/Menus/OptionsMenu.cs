c7ae83429117aa05c795ad4b2a8f5e9b3b20e94e
AlbaRonSe2B <albaroncero_bcnvj1ma1920@cev.com>
2021-02-11 18:57:58 +0100

    ahora sí que sí, brightness BIEN

--- Assets/Scripts/Menus/OptionsMenu.cs
+++ Assets/Scripts/Menus/OptionsMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7db94dd8d041d7a5b7269419ae04001726e18bddd0797d36565d6f923a1850c
+oid sha256:b967a33ba937e2bcb46d310a8b2fd041af30e2b491ea60bb6e2c69d79dd847ec
 size 8593
