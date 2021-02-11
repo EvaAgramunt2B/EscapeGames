5ae5eecfb44aac6e6fee4f1154e74470feed1576
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-02-11 14:05:00 +0100

    gamma option (aunque no sé que es lo que cambia)

--- Assets/Scripts/Menus/OptionsMenu.cs
+++ Assets/Scripts/Menus/OptionsMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a5b3603fd76764d7b011d113ee74d37d084eaf2910620143ea0137ef692dc68
-size 7826
+oid sha256:1da79fb42ba26203eb35c6628d7727acdaad74e652adf973531c223806800cbc
+size 7822
