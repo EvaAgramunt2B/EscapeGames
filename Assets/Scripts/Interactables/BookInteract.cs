39b4a8c3daad181891864a85212bfa201a310df8
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-03-04 18:32:54 +0100

    libro tamb funciona con raycast

--- Assets/Scripts/Interactables/BookInteract.cs
+++ Assets/Scripts/Interactables/BookInteract.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9fdcaff6c3a6ceb28435e146a7bd8d3302e4f02951e3d070e77182417a088a0
-size 2621
+oid sha256:493f601ec8fcbdac4a59744306b46fe70c9a940c155ecd28a0e1746059af57ca
+size 2952
