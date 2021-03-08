abcfe454a7121d53e545f3f2f3582d5339659463
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-03-08 16:45:21 +0100

    raycast funciona bro

--- Assets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
+++ Assets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d51eb22ac22efcf178d5963fbe906dadf83573abc4280f1d9d5ae14ea1f4837c
+oid sha256:634d64397560a267fc5f86ba24c85ad8aefd99c9eec916cbf8751a28ea55924f
 size 5230
