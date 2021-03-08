6231db628cc113f5bd0796c9ed5ffb7ea1e2fcde
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-03-08 16:49:19 +0100

    se ve el cursor en victoria y derrota

--- Assets/Scripts/Menus/GameOverMenu.cs
+++ Assets/Scripts/Menus/GameOverMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd70d94ae59a22fe6715a73251c6f38c3aa9f07de1eea89d7f61fc545b65199c
-size 640
+oid sha256:0b40978502ec6d0a3af2dc31f63d3c383eb6e5b23df0cb900208c4209e7fcde7
+size 672
