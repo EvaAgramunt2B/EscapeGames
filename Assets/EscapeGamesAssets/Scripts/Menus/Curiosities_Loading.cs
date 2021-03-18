71b076b244c212a5dd182a163917c2d60c1d6b5b
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-03-18 19:03:52 +0100

    loading screen con curiosidades

---
+++ Assets/EscapeGamesAssets/Scripts/Menus/Curiosities_Loading.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ebbf4bf34aa2cd8fdd005170d4df684560f8ef174baa6974e80840d880b1e31e
+size 4190
--- Assets/EscapeGamesAssets/Scripts/Menus/SceneLoading.cs
+++ Assets/EscapeGamesAssets/Scripts/Menus/SceneLoading.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7763567adc3854acecf75ae57e3ab54f661e4213a0498eee0633b03c2ebc232e
-size 775
+oid sha256:d4c6c47ca566a4763f806c070444eaf7b62b5b17f5efb2fb2f97ac62da420046
+size 2452
