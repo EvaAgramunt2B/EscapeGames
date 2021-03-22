7b6b5567f481a1b25e17b7d1b958691c75c59829
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-03-22 16:48:24 +0100

    interactuar input system

--- Assets/EscapeGamesAssets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
+++ Assets/EscapeGamesAssets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6c615e20354ee61140591ada7e40b0148aefd481608d3809f7f5f194e0eb9e9
-size 8160
+oid sha256:403fa0a09520435d0de6781187b30843209e176da2f29fbf4d5dc4b2548b7dee
+size 8516
