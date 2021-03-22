39d1a88d26695c0a04ab3782221fa983ed0efc1e
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-03-22 17:56:12 +0100

    arregladas cosas que habian petado wtf

--- Assets/EscapeGamesAssets/Scripts/Interactables/Observe.cs
+++ Assets/EscapeGamesAssets/Scripts/Interactables/Observe.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ca9ebf5d421ad376d96dbe7618268ca8f1e3d186478debb6aa7b915cf4ff1ff
-size 3006
+oid sha256:c33688f8f70ce8e5c3eb3d2e4922ca46e79705de983c2f2ddf20191a76dc8b44
+size 2970
