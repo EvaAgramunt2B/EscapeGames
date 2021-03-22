e51cc4a22dea1b38f69ad0568156268c12274774
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-03-22 16:57:55 +0100

    mouse movement en pc ya no hace parkour

--- Assets/EscapeGamesAssets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
+++ Assets/EscapeGamesAssets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:403fa0a09520435d0de6781187b30843209e176da2f29fbf4d5dc4b2548b7dee
-size 8516
+oid sha256:e91bdf45703f4de0ab15df8a0022b9432b242e677c384d1ef72edffef3dace9b
+size 9268
