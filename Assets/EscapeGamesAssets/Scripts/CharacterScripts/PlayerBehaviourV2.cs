96b3db10f4663909a576ac8cd511b5aff414ec9e
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-04-06 16:20:53 +0200

    blend distance y no se puedeganar xd

--- Assets/EscapeGamesAssets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
+++ Assets/EscapeGamesAssets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74cd03081b0cd1f992c8f14d6e54726626af924341eb2cefc03d251c822c88b1
-size 8260
+oid sha256:89182577038a3cd88a7c6f9ac7a92c5ab04a7bce6722f521a2d097cbf51cf989
+size 8264
