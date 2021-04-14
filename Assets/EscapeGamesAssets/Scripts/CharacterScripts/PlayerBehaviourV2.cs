6e217ff9faea299c34cb30edc1685b2066a52634
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-04-14 18:09:14 +0200

    espejo en el baño

--- Assets/EscapeGamesAssets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
+++ Assets/EscapeGamesAssets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ddc4c0dbfc9b74bd0f8635da6980d499dd73780fcde1caf5d57db23f2a7a48d
+oid sha256:18ddbbf3f37d237d9388c54e2b97c678ce53b76403dc8e64e3eae2205ef17270
 size 8424
