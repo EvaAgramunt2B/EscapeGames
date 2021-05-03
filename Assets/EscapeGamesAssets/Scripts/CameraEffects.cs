a8381f2a3fe323a28d9bf0b3aded51b8798b1594
AlbaRonSe2B <albaroncero_bcnvj1ma1920@cev.com>
2021-05-04 01:49:52 +0200

    cambiado luces del garaje que estaban demasiado fuertes

--- Assets/EscapeGamesAssets/Scripts/CameraEffects.cs
+++ Assets/EscapeGamesAssets/Scripts/CameraEffects.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a86d3edd030aed31e302c7ca3ca28183db9a74e85e334551abf00385ff1e658f
-size 626
+oid sha256:745da6de9662bc367d3cc0516983de6b8b1a1b588a3eea5a26b36248608d542d
+size 630
