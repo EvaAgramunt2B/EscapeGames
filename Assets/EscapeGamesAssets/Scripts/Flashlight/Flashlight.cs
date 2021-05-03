e8f4fec970b1e86ddd2d8a31ddbed6373c7bcd92
AlbaRonSe2B <albaroncero_bcnvj1ma1920@cev.com>
2021-05-04 00:59:33 +0200

    linterna flicker, en proseso wey

--- Assets/EscapeGamesAssets/Scripts/Flashlight/Flashlight.cs
+++ Assets/EscapeGamesAssets/Scripts/Flashlight/Flashlight.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94a2388b2f5180786bfe885f5d31cef6b096a13b227daf1d0332386a44f4b44b
-size 2925
+oid sha256:5254d0fcc55679e83382e6e0ac6b7b754a4ef83d0c1727ad9935c1054d7ddf87
+size 3604
