962cd8c3a7111a7accddf0a5286d3419ea1675c2
Eva <evaagramunt_bcnvj1ma1920@cev.com>
2021-05-06 18:32:23 +0200

    keys model changed

--- Assets/EscapeGamesAssets/Scripts/Keys/PuertasJardin_KEYS.cs
+++ Assets/EscapeGamesAssets/Scripts/Keys/PuertasJardin_KEYS.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e12953542bdf4bfc9e9254c63d50d7e2ed1c8b014cfb398f18f4e2463bb1769a
-size 2886
+oid sha256:a53fbc242dce9ce787824a2a1ced0709f25b4f9b98304fca180780f8b8082f4e
+size 1383
--- Assets/EscapeGamesAssets/Scripts/Keys/TraseroCaja_KEY.cs
+++ Assets/EscapeGamesAssets/Scripts/Keys/TraseroCaja_KEY.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bbb9f02e337cc915e53c58fe9ab55cbd5ef5b1919aa424115dcff0407eb4030
-size 2888
+oid sha256:51fc930362e4096c855f9d41cbb4a64f5c3325f8910ddefe82c6f9d1b1e703e2
+size 1376
