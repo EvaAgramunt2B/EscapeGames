1df1142e491f70abc6122c9e014ca909a22dfae8
Eva <evaagramunt_bcnvj1ma1920@cev.com>
2021-05-06 17:47:28 +0200

    sonido highlight buttons TESTING

--- Assets/EscapeGamesAssets/Scripts/Menus/MainMenuManager.cs
+++ Assets/EscapeGamesAssets/Scripts/Menus/MainMenuManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74d5f6ebc9e2ebd404654a298f1a246b4bef04d0f2c8ee6845ec3b9ffb160c9e
-size 8378
+oid sha256:35e4ff6ce0a3e63e737048d255770a0b1e1d0f732fc905ef93f2f753bbe96dc2
+size 8188
--- Assets/EscapeGamesAssets/Scripts/Puzzles/LightsController.cs
+++ Assets/EscapeGamesAssets/Scripts/Puzzles/LightsController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:356187c772f0cbf039c1ebd3c0e5ee844a79beb738df2d4e2d16802d898c82a5
-size 1219
+oid sha256:d226d1ac246c87d4501e8d331c72c702bf792fcefd377b9790d57cd95e6f25e2
+size 1220
