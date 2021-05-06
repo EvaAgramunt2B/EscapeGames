947f5516be999d6a4b7c5b82ede83561e23575d4
Eva <evaagramunt_bcnvj1ma1920@cev.com>
2021-05-06 18:55:26 +0200

    emily hablando en algnas cosillas + eliminado boton on/off de la tv

--- Assets/EscapeGamesAssets/Scripts/Doors/BasementDoor.cs
+++ Assets/EscapeGamesAssets/Scripts/Doors/BasementDoor.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13c8b6be0dda982920a9c7afb50dfe08d33543a4cb0e0a8035981e49b74daaa7
-size 2937
+oid sha256:b73f462b04f7b60695a55b88936bd31aa3f7f0474e683a852fc3832ce4d407c5
+size 1582
--- Assets/EscapeGamesAssets/Scripts/Doors/OutsideDoors.cs
+++ Assets/EscapeGamesAssets/Scripts/Doors/OutsideDoors.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:788d6eaff844c0530e2d43bdfa8459e453e69b0d3d9a379e2b7f24a6821f6e44
-size 3303
+oid sha256:305fbbad2e8f1c44313ba13f6b05659c32b4dee9e9ae71b603750ec27828f667
+size 1677
