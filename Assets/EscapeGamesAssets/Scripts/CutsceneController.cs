7edf957ae4fcfea699e958c45684c6828437aea4
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-05-25 15:35:52 +0200

    cutscene cambia pantalla

---
+++ Assets/EscapeGamesAssets/Scripts/CutsceneController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2d32dc29fe253a9ffdf76209a3538aa36343974c0027fde313c47d7d01577a83
+size 385
--- Assets/EscapeGamesAssets/Scripts/Menus/IntroManager.cs
+++ Assets/EscapeGamesAssets/Scripts/Menus/IntroManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94d2f63cb15fae1023587d77ec760e120fd3ec02eb1d3abddf4ba02d1a03b5aa
-size 1332
+oid sha256:9e2a3ba1c5ff5dfc3d0cac650cbbf38916514b2ca0a37b95919b14ce44b48140
+size 1350
