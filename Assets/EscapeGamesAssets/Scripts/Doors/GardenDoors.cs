a6fb1bb5cac71652a064cd0cec62c2131a7ca93b
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-05-27 17:30:44 +0200

    pushhh

--- Assets/EscapeGamesAssets/Scripts/Doors/GardenDoors.cs
+++ Assets/EscapeGamesAssets/Scripts/Doors/GardenDoors.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02461b3a23dedf3834f1092c1cfb4d83b54f25f94bb4b7028cb5c27529927f9c
-size 1669
+oid sha256:ab902c5a9572ec1ebb49c6b0454001a07a2a3812dddb3a2286c3f448563d9505
+size 1728
--- Assets/EscapeGamesAssets/Scripts/Doors/OutsideDoors.cs
+++ Assets/EscapeGamesAssets/Scripts/Doors/OutsideDoors.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:305fbbad2e8f1c44313ba13f6b05659c32b4dee9e9ae71b603750ec27828f667
-size 1677
+oid sha256:eecbd37afce9c4a69cbc0cf0b2a1fd72a346ac56b770a59eb1e1949aedb9a280
+size 1567
