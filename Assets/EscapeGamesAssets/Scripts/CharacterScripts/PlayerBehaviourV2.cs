a80a549cca809c182ea5635809bd57b569c65c25
EvaAgramunt2B <evaagramunt_bcnvj1ma1920@cev.com>
2021-05-27 16:33:21 +0200

    bug restart ramirez arreglado

--- Assets/EscapeGamesAssets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
+++ Assets/EscapeGamesAssets/Scripts/CharacterScripts/PlayerBehaviourV2.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b6bd4857dd65615dbbcf3c249b562f4dea44b8cc6e8622b400ea2084ad1a1d5
-size 8763
+oid sha256:f4bd9808fc233d959f4a04f1a30a536476f0b4dbfbf386198694ff0458b7d49f
+size 8805
