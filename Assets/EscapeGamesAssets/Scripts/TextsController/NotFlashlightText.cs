122d161da1f0259bcd1932d9b13d556799ad172f
Alba <evaagramunt_bcnvj1ma1920@cev.com>
2021-06-01 19:05:55 +0200

    audio cuando se olvida la linterna en el basement

---
+++ Assets/EscapeGamesAssets/Scripts/TextsController/NotFlashlightText.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7166717e62a14dbf8030c97fb1a689ab0ac0a1a6b6f8296d7c9dcfcb1c868c84
+size 1187
--- Assets/EscapeGamesAssets/Scripts/TextsController/TalkingEmilyScript.cs
+++ Assets/EscapeGamesAssets/Scripts/TextsController/TalkingEmilyScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4e179aa564360a6f21efd00eb900484a101af76ba8a6e0f613d5bd98dcd425a
-size 1566
+oid sha256:8f9560067cda3e95dcde72240e5522dab57e78423ad2ad98b1ea3cf263a6ec40
+size 1581
