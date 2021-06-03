2e0865e3fa78ac8392c5aa1d447e69bdd34adb34
Alba <evaagramunt_bcnvj1ma1920@cev.com>
2021-06-03 16:44:09 +0200

    ya no puedes llamar tu a ramirez xdd

--- Assets/EscapeGamesAssets/Scripts/PhoneCall.cs
+++ Assets/EscapeGamesAssets/Scripts/PhoneCall.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dea6051f024801696b5aa6a2f1a16f956152556095bee3041afaffca2730e13
-size 1536
+oid sha256:a6d56647d67b4b4be9050a99ec7ee876e045130b6e574d4b897aecd9ef0dcca6
+size 1641
