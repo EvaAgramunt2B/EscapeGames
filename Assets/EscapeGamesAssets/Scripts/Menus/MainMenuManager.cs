4c3375b4312ca0a38139be7ed89e013295dd42d7
Alba <evaagramunt_bcnvj1ma1920@cev.com>
2021-06-03 16:58:03 +0200

    credits puestos

--- Assets/EscapeGamesAssets/Scripts/Menus/MainMenuManager.cs
+++ Assets/EscapeGamesAssets/Scripts/Menus/MainMenuManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:459cf37fdb848f74311e618a6a33a816e5e1e1b5e03041bc3c630648c72846f0
-size 8298
+oid sha256:3869dbfb82fb842fec4ffa581468623d9708f4a7ae913e61132761d6036d3db0
+size 8540
