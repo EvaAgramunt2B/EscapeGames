e3185760bb758e8fd144314c131fae1d59be4aeb
Alba <evaagramunt_bcnvj1ma1920@cev.com>
2021-06-03 16:35:15 +0200

    audios llamada

--- Assets/EscapeGamesAssets/Scripts/PhoneCall.cs
+++ Assets/EscapeGamesAssets/Scripts/PhoneCall.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:008e37aed00e29a85e0537a1ec716f4b3c4020a9d3bda39899ad6518bd6e5778
-size 1406
+oid sha256:2dea6051f024801696b5aa6a2f1a16f956152556095bee3041afaffca2730e13
+size 1536
